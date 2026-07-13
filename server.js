c400d99cc9ba80d1ab0db41da17d202db7fcd5c5
ddnya2857-rgb <____GitHub>
2026-07-13 17:09:44 +0100

    fix port binding

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/get-users', (req, res) => {
 
 // إعداد المنفذ للعمل محلياً أو على Render
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 const nodemailer = require('nodemailer');
